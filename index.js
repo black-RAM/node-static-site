e337713722b4fb698030a3b4ea56c25e344923e5
black-RAM <pplanes311@gmail.com>
2024-08-22 15:49:31 +0300

    routing done, project done :)

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const http = require("node:http")
+const fs = require("node:fs")
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {"x-content-type-options": "text/html"})
-  res.end("Hello, world!")
+  const routes = {
+    "/": "./index.html",
+    "/about": "./about.html",
+    "/contact-me": "./contact-me.html",
+    "/styles.css": "./styles.css"
+  }
+
+  const fileName = routes[req.url] || "404.html"
+  const fileExt = fileName.match(/\.\w+$/)
+  const readerConfig = { encoding: "utf8" }
+  const readerCallback = (err, data) => {
+    if(err) {
+      console.error(err)
+      return
+    }
+    res.end(data)
+  }
+
+  res.writeHead(200, {"x-content-type-options": "text/" + fileExt})
+  fs.readFile(fileName, readerConfig, readerCallback)
 })
 
 server.listen(8080)
