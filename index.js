628861406d97bd6eabf29064deb960206bc92d2b
black-RAM <pplanes311@gmail.com>
2024-08-23 19:51:01 +0300

    express framework refactor

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-const http = require("node:http")
-const fs = require("node:fs")
+const express = require("express")
+const app = express()
+const PORT = 8080
 
-const server = http.createServer((req, res) => {
-  const routes = {
-    "/": "./index.html",
-    "/about": "./about.html",
-    "/contact-me": "./contact-me.html",
-    "/styles.css": "./styles.css"
-  }
+const htmlSender = (res, fileNameWithoutExtension) => {
+  res.sendFile("./src/" + fileNameWithoutExtension + ".html", { root: __dirname })
+}
 
-  const fileName = routes[req.url] || "404.html"
-  const fileExt = fileName.match(/\.\w+$/)
-  const readerConfig = { encoding: "utf8" }
-  const readerCallback = (err, data) => {
-    if(err) {
-      console.error(err)
-      return
-    }
-    res.end(data)
-  }
+app.use(express.static("public"))
+app.get("/", (_, res) => htmlSender(res, "index"))
+app.get("/about", (_, res) => htmlSender(res, "about"))
+app.get("/contact-me", (_, res) => htmlSender(res, "contact-me"))
+app.all("*", (_, res) => htmlSender(res, "404"))
 
-  res.writeHead(200, {"x-content-type-options": "text/" + fileExt})
-  fs.readFile(fileName, readerConfig, readerCallback)
-})
-
-server.listen(8080)
+app.listen(PORT)
